05e04f05671c4fa8da5c6fcb4ba49231eb4a5b4f
Shawn Anderson <shawn42@gmail.com>
2012-06-15 17:46:20 -0400

    closer

--- src/behaviors/foxy_collision_points.rb
+++ src/behaviors/foxy_collision_points.rb
@@ -20,19 +20,20 @@ define_behavior :foxy_collision_points do
       quarter_h = h * 0.25
       three_quarter_h = h - quarter_h
       three_quarter_w = w - quarter_w
+      offset = 2
 
       [
-        vec2(x+quarter_w,       y),
-        vec2(x+three_quarter_w, y),
+        vec2(x+offset, y),
+        vec2(x+w-offset, y),
 
-        vec2(x+w,               y+quarter_h),
-        vec2(x+w,               y+three_quarter_h),
+        vec2(x+w,               y+offset),
+        vec2(x+w,               y+h-3-offset),
 
-        vec2(x+three_quarter_w, y+h-3),
-        vec2(x+quarter_w,       y+h-3),
+        vec2(x+offset, y+h-3),
+        vec2(x+w-offset,       y+h-3),
 
-        vec2(x,                 y+three_quarter_h),
-        vec2(x,                 y+quarter_h),
+        vec2(x,                 y+offset),
+        vec2(x,                 y+h-3-offset),
       ]
     end
   end
