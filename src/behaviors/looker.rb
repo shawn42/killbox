2ded24adc1c671c510ac2912a4e0795f02950f49
Shawn Anderson <shawn42@gmail.com>
2012-10-28 01:44:44 -0400

    smoother looker behavior

--- src/behaviors/looker.rb
+++ src/behaviors/looker.rb
@@ -6,24 +6,16 @@ define_behavior :looker do
     actor.has_attributes look_distance: 100,
                          flip_h: false
 
-    director.when :update do |time_in_ms|
-      fade_look_point time_in_ms
+    director.when :update do |t_ms, time_in_sec|
+      update_look_point time_in_sec
     end
 
     input = actor.input
     input.when :look_left do
-      look look_directions[:left]
       actor.flip_h = true
     end
     input.when :look_right do
-      look look_directions[:right]
       actor.flip_h = false
-    end
-    input.when :look_up do
-      look look_directions[:up]
-    end
-    input.when :look_down do
-      look look_directions[:down]
     end
   end
 
@@ -38,16 +30,31 @@ define_behavior :looker do
       }
     end
 
-    def fade_look_point(time_ms)
-    end
+    def update_look_point(time_secs)
+      input = actor.input
 
-    def look(look_vector)
-      # TODO do we need to smooth this out?
-      rot = actor.do_or_do_not(:rotation) || 0
-      vp_offset = -look_vector.rotate_deg(rot) * actor.look_distance
+      look_vector = if input.look_left?
+        look_directions[:left]
+      elsif input.look_right?
+        look_directions[:right]
+      elsif input.look_up?
+        look_directions[:up]
+      elsif input.look_down?
+        look_directions[:down]
+      end
 
-      actor.viewport.follow_offset_x = vp_offset.x
-      actor.viewport.follow_offset_y = vp_offset.y
+      viewport = actor.viewport
+      offset_vec = vec2(viewport.follow_offset_x, viewport.follow_offset_y)
+      if look_vector 
+        rot = actor.do_or_do_not(:rotation) || 0
+        offset_vec -= look_vector.rotate_deg(rot) * actor.look_distance * time_secs
+      else
+        offset_vec = vec2(0,0) 
+      end
+
+      offset_vec.magnitude = actor.look_distance if offset_vec.magnitude > actor.look_distance
+      viewport.follow_offset_x = offset_vec.x
+      viewport.follow_offset_y = offset_vec.y
     end
 
   end
