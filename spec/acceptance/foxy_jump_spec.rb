509f219f6b775524f172132641710f2e9cbff20f
Shawn Anderson <shawn42@gmail.com>
2013-04-14 12:00:15 -0400

    merge

--- spec/acceptance/foxy_jump_spec.rb
+++ spec/acceptance/foxy_jump_spec.rb
@@ -151,10 +151,6 @@ describe "Foxy jumping", acceptance: true do
         y: floor_y.ish,
         rotation: 0.ish
 
-      foxy.when :vel do |old, n|
-        binding.pry
-      end
-
       jump 10
       update 20
 
@@ -200,37 +196,6 @@ describe "Foxy jumping", acceptance: true do
       foxy.y.should be < floor_y
 
     end
-  end
-
-  def jump(time_held)
-    # charge & jump
-    hold_key KbN, time_held, step: 20
-  end
-
-  def charge_and_throw_bomb(time_held)
-    hold_key KbM, time_held, step: 20
-  end
-
-  def walk_left(time_held)
-    hold_key KbA, time_held, step: 20
-  end
-
-  def walk_right(time_held)
-    hold_key KbD, time_held, step: 20
-  end
-
-  def look_up
-    press_key KbW
-  end
-
-  def shields_up
-    press_key KbV
-  end
-
-  def hold_key(key, time_held, opts={})
-    press_key key
-    update time_held, step: opts[:step]
-    release_key key
   end
 
 end
