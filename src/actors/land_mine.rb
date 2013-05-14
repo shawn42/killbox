0d32f64530c2a26475ea31921389c1773c836a82
Shawn Anderson <shawn42@gmail.com>
2013-05-14 11:19:09 -0400

    Un-animates land mines to better hide them

--- src/actors/land_mine.rb
+++ src/actors/land_mine.rb
@@ -4,7 +4,7 @@ define_actor :land_mine do
     positioned
     audible
     layered ZOrder::PlayerDecoration
-    animated_with_spritemap file: 'trippers/props.png', rows: 3, cols: 6, actions: { idle: 0..2 }
+    animated_with_spritemap file: 'trippers/props.png', rows: 3, cols: 6, actions: { idle: 0 }
     bound_by_box
     bomb_collision_points
     explode_by_bomb
