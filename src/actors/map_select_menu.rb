c0343e29946ad5d8ffcafc6fae9ac3202805d68a
Jason Porritt <jason.porritt@atomicobject.com>
2013-05-22 13:06:09 -0400

    Changes menu layout to 4 tiles wide.

--- src/actors/map_select_menu.rb
+++ src/actors/map_select_menu.rb
@@ -20,14 +20,13 @@ define_actor :map_select_menu do
           stage.create_actor(:label, text: map_count+1, x: icon_x-icon_width / 2+50, y: icon_y-icon_height/2, font_name: "vigilanc.ttf" , font_size: (icon_height/2), color: [244, 215, 227])
           input_manager.reg :down, Object.const_get("Kb#{map_count+1}") do actor.emit :start, map.to_sym end
 
-
-          if map_count.odd?
+          map_count += 1
+          if map_count % 4 == 0
             icon_y += icon_height
             icon_x = left_x
           else
             icon_x += icon_width
           end
-          map_count += 1
         end
       end
 
