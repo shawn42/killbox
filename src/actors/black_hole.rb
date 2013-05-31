29def093d45ec3268dc4b3398ee32d6c67a85714
Shawn Anderson <shawn42@gmail.com>
2013-05-30 23:04:25 -0400

    Adds basic animation to black hole

--- src/actors/black_hole.rb
+++ src/actors/black_hole.rb
@@ -1,11 +1,23 @@
 define_actor :black_hole do
   has_behaviors do
     layered ZOrder::BackgroundEffects
-    animated_with_spritemap file: 'trippers/black_hole.png', rows: 1, cols: 1, actions: { idle: 0 }
+    animated_with_spritemap file: 'trippers/black_hole.png', rows: 1, cols: 3, actions: { idle: 0..1 }
     emits_gravity
   end
 
-  # TODO clean this up?
+  # rotation needs to go somewhere better than tile_bouncer
+  behavior do
+    requires :director
+
+    setup do
+      actor.has_attributes rotation: 0, rotation_vel: 1.3
+      director.when :update do |time_delta_ms|
+        actor.rotation += actor.rotation_vel
+      end
+    end
+  end
+
+  # TODO clean this up? shared view code .. 
   view do
     draw do |target, x_off, y_off, z|
       x = actor.x
